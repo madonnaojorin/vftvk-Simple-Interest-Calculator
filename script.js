61c6353cd9539633e1277f27976c2a3c0187c8ba
Nao Yamamoto <64042180+madonnaojorin@users.noreply.github.com>
2022-04-15 13:55:30 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var year = parseInt(document.getElementById("year").value);
+    var interest = principal * year * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + year;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 }
-        
